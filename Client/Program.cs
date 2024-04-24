c7f14439e880b0c92fe9ddfea898708da2ca4424
yoonbigbear <yoonbigbear@hotmail.com>
2024-04-25 01:34:15 +0900

    기본 서버 통신 시도

---
+++ Client/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Client
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			string Ip = "127.0.0.1";
+			int Port = 11000;
+			System.Net.Sockets.TcpClient tcpClient = new();
+
+			tcpClient.ConnectAsync(Ip, Port).Wait();
+			if (tcpClient.Connected)
+			{
+				Console.WriteLine($"Server Connected");
+			}
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Project
+{
+	using System.Net;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			string Ip = "127.0.0.1";
+			int Port = 11000;
+
+			System.Net.Sockets.TcpListener tcpListener = new(IPAddress.Parse(Ip), Port);
+			tcpListener.Start();
+
+			tcpListener.AcceptSocketAsync().Wait();
+
+			Console.WriteLine($"Accept Client");
+			while (true)
+			{
+				Thread.Sleep(3000);
+			}
+		}
+	}
+}
