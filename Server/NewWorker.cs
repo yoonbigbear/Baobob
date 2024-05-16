62f885948a6d6feb692a52db692c7fa46609a0e2
yoonbigbear <yoonbigbear@hotmail.com>
2024-05-17 01:36:06 +0900

    동시성 큐 추가
    - ParallelSingleWorker는 동시에 여러개 실행이 가능
    - SequentialSingleWorker는 동시에 1개만 실행 가능

---
+++ Server/NewWorker.cs
@@ -0,0 +1,38 @@
+﻿namespace Server
+{
+	public class ParallelSingleWorker
+	{
+		private ConcurrentExclusiveSchedulerPair ConcurrentExclusiveSchedulerPair;
+
+		private TaskFactory factory;
+
+		private CancellationToken CancellationToken;
+
+		public ParallelSingleWorker()
+		{
+			this.factory = new TaskFactory(new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, 10).ConcurrentScheduler);
+		}
+
+		public Task TryQueue(Action action)
+		{
+			return factory.StartNew(action);
+		}
+	}
+
+	public class SequentialSingleWorker
+	{
+		private ConcurrentExclusiveSchedulerPair ConcurrentExclusiveSchedulerPair;
+		private TaskFactory factory;
+		private CancellationToken CancellationToken;
+
+		public SequentialSingleWorker()
+		{
+			this.factory = new TaskFactory(new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, 1).ExclusiveScheduler);
+		}
+
+		public Task TryQueue(Action action)
+		{
+			return factory.StartNew(action);
+		}
+	}
+}
