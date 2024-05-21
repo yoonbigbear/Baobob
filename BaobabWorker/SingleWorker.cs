b11c003f664ffe611d4613b21fae37e644703072
yoonbigbear <yoonbigbear@hotmail.com>
2024-05-22 01:22:33 +0900

    Baobob 워커 추가
    - 싱글 워커

---
+++ BaobabWorker/SingleWorker.cs
@@ -0,0 +1,20 @@
+﻿namespace BaobabWorker
+{
+	/// <summary>
+	/// 동시에 하나의 작업만 실행할 수 있도록 보장한다.
+	/// </summary>
+	public class SingleWorker
+	{
+		private TaskFactory factory;
+
+		public SingleWorker()
+		{
+			this.factory = new TaskFactory(new ConcurrentExclusiveSchedulerPair().ExclusiveScheduler);
+		}
+
+		public virtual Task TryQueue(Action action)
+		{
+			return factory.StartNew(action);
+		}
+	}
+}
