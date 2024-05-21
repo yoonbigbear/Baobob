9dfd7bf493ab395e0f6c55eb9c41a6ad219b1ade
yoonbigbear <yoonbigbear@hotmail.com>
2024-05-22 01:23:23 +0900

    서버 프로젝트 업데이트

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,37 +1,13 @@
 ﻿namespace Project
 {
-	using System.Net;
-	using System.Text;
-
 	internal class Program
 	{
-		private static async Task Main(string[] args)
+		private static void Main(string[] args)
 		{
-			string Ip = "127.0.0.1";
-			int Port = 11000;
-
-			System.Net.Sockets.TcpListener tcpListener = new(IPAddress.Parse(Ip), Port);
-			tcpListener.Start();
-
-			//Session
-			{
-				Console.WriteLine($"Accept Client");
-				await Listen(tcpListener);
-			}
-
 			while (true)
 			{
-				await Task.Delay(3000);
+				Thread.Sleep(1000);
 			}
-		}
-
-		private static async Task Listen(System.Net.Sockets.TcpListener tcpListener)
-		{
-			var socket = await tcpListener.AcceptSocketAsync();
-
-			byte[] bytes = new byte[1024];
-
-			await socket.SendAsync(Encoding.UTF8.GetBytes("Hello"));
 		}
 	}
 }
