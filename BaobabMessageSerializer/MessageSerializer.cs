d196d4149d01a1eccfb54252cfabbc3f87ffd8c6
yoonbigbear <yoonbigbear@hotmail.com>
2024-05-26 17:01:44 +0900

    플랫버퍼 추가

---
+++ BaobabMessageSerializer/MessageSerializer.cs
@@ -0,0 +1,38 @@
+﻿namespace BaobabMessageSerializer
+{
+	public class BaobabSerializableAttribute : Attribute
+	{
+	}
+
+	public class MessageSerializer
+	{
+	}
+
+	[BaobabSerializable]
+	public struct Message2Args : IMessage
+	{
+		public int MessageID => throw new NotImplementedException();
+
+		public int arg1 { get; set; }
+
+		public string arg2 { get; set; }
+	}
+
+	[BaobabSerializable]
+	public struct Message3Args : IMessage
+	{
+		public int MessageID => throw new NotImplementedException();
+
+		public int arg1 { get; set; }
+
+		public string arg2 { get; set; }
+
+		public List<float> arg3 { get; set; }
+	}
+
+	public enum MessageID
+	{
+		Unknown,
+		End,
+	}
+}
---
+++ Common/compile_flatc.bat
@@ -0,0 +1,1 @@
+flatc --csharp packet.fbs
