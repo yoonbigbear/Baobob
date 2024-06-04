d0377ad565beac5dcf5fac36024a9807faf0e114
yoonbigbear <yoonbigbear@hotmail.com>
2024-06-05 00:18:03 +0900

    RPC 추가 도중

---
+++ BaobabRPC/BaobabRPCAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace BaobabRPC
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public class BaobabRPCIDLAttribute : Attribute
+	{
+	}
+
+	public class RPCProxy
+	{
+	}
+
+	public class RPCHelper<T>
+	{
+		public static List<MemberInfo> GetIDLMemberInfo()
+		{
+			List<MemberInfo> list = new List<MemberInfo>();
+			// 어셈블리 내의 모든 타입 가져온다.
+			Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+			// 각 타입을 순회하면서 [BaobabRPCIDL]를 사용 중인 메서드 검사
+			foreach (Type type in types)
+			{
+				var interfaces = type.GetInterfaces();
+
+				foreach (var itf in interfaces)
+				{
+					if (itf.GetCustomAttributes(typeof(BaobabRPCIDLAttribute), false).Length > 0)
+					{
+						list.AddRange(itf.FindMembers(MemberTypes.Method, BindingFlags.Default, null, null));
+					}
+				}
+			}
+			return list;
+		}
+	}
+
+	public interface ISampleRPC
+	{
+		[BaobabRPCIDL]
+		public void SampleRPC();
+	}
+}
---
+++ BaobabRPC/Stub.cs
@@ -0,0 +1,15 @@
+﻿namespace BaobabRPC
+{
+	internal class Stub
+	{
+		public int Unmarhsal(byte[] bytes)
+		{
+			return 0;
+		}
+
+		public int Marshal(byte[] bytes)
+		{
+			return 0;
+		}
+	}
+}
