823a44e1f0e5cb5a3fe789219ea9641efc997a91
yoonbigbear <yoonbigbear@hotmail.com>
2024-06-11 01:16:56 +0900

    RSA+AES / RSA+XOR
    암호화 추가

---
+++ BaobabUnitTest/BaobabNetwork/NetworkUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace BaobabNetwork
+{
+	[TestClass]
+	public class NetworkUnitTest
+	{
+		[TestMethod]
+		public void RSAXorPublicKey()
+		{
+			// 서버 생성 및 공개 키 출력
+			ServerRSA server = new();
+			string publickey = server.PublicKey;
+
+			// 클라이언트 생성 및 대칭 키 생성
+			ClientRSA client = new ClientRSA(publickey);
+			byte[] symmetricKey = client.GenerateSymmetricKey();
+
+			// 대칭 키 암호화 및 서버로 전송 (여기서는 로컬 시뮬레이션)
+			byte[] encryptedSymmetricKey = client.EncryptData(symmetricKey);
+			byte[] decryptedSymmetricKey = server.DecryptData(encryptedSymmetricKey);
+
+			// 서버와 클라이언트의 대칭 키 확인
+			string symmetricString = Convert.ToBase64String(symmetricKey);
+			string decryptedSymmetricString = Convert.ToBase64String(decryptedSymmetricKey);
+			Assert.AreEqual(decryptedSymmetricString, symmetricString);
+
+			// 데이터를 XOR 방식으로 암호화
+			string originalMessage = "Hello, World!";
+			byte[] messageBytes = Encoding.UTF8.GetBytes(originalMessage);
+			byte[] encryptedMessage = XorBase.XorEncrypt(messageBytes, decryptedSymmetricKey);
+
+			// 암호화된 메시지
+			string encryptedText = Convert.ToBase64String(encryptedMessage);
+
+			// 메시지 복호화
+			byte[] decryptedMessage = XorBase.XorEncrypt(encryptedMessage, decryptedSymmetricKey);
+			string decryptedText = Encoding.UTF8.GetString(decryptedMessage);
+
+			// 복호화된 메시지
+			Assert.AreEqual(originalMessage, decryptedText);
+		}
+
+		[TestMethod]
+		public void RSAAESPublicKey()
+		{
+			// 서버 생성 및 공개 키 출력
+			ServerRSA server = new();
+			string publickey = server.PublicKey;
+
+			// 클라이언트 생성 및 대칭 키 생성
+			ClientRSA client = new ClientRSA(publickey);
+			byte[] symmetricKey = client.GenerateSymmetricKey();
+
+			// 대칭 키 암호화 및 서버로 전송 (여기서는 로컬 시뮬레이션)
+			byte[] encryptedSymmetricKey = client.EncryptData(symmetricKey);
+			byte[] decryptedSymmetricKey = server.DecryptData(encryptedSymmetricKey);
+
+			// 서버와 클라이언트의 대칭 키 확인
+			string symmetricString = Convert.ToBase64String(symmetricKey);
+			string decryptedSymmetricString = Convert.ToBase64String(decryptedSymmetricKey);
+			Assert.AreEqual(decryptedSymmetricString, symmetricString);
+
+			// 데이터 AES 암호화 및 전송
+			string originalMessage = "Hello, World!";
+			byte[] messageBytes = Encoding.UTF8.GetBytes(originalMessage);
+			byte[] encryptedMessage = AESBase.EncryptWithAES(messageBytes, decryptedSymmetricKey);
+
+			// 암호화된 메시지
+			string encryptedText = Convert.ToBase64String(encryptedMessage);
+
+			// 메시지 복호화
+			byte[] decryptedMessage = AESBase.DecryptWithAES(encryptedMessage, decryptedSymmetricKey);
+			string decryptedText = Encoding.UTF8.GetString(decryptedMessage);
+
+			// 복호화된 메시지
+			Assert.AreEqual(originalMessage, decryptedText);
+		}
+	}
+}
---
+++ BaobobNetwork/Encryption/RSABase.cs
@@ -0,0 +1,72 @@
+﻿namespace BaobabNetwork
+{
+	using System.Security.Cryptography;
+	using System;
+	using System.Collections.Generic;
+
+	public class RSAKeyPair
+	{
+		public string PublicKey { get; private set; }
+		public string PrivateKey { get; private set; }
+
+		public RSAKeyPair()
+		{
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+				PublicKey = Convert.ToBase64String(rsa.ExportCspBlob(false));
+				PrivateKey = Convert.ToBase64String(rsa.ExportCspBlob(true));
+			}
+		}
+	}
+
+	public class ServerRSA
+	{
+		private RSACryptoServiceProvider rsa;
+		public string PublicKey { get; private set; }
+		private string PrivateKey { get; set; }
+
+		public ServerRSA()
+		{
+			rsa = new RSACryptoServiceProvider(2048);
+			rsa.PersistKeyInCsp = false;
+			PublicKey = Convert.ToBase64String(rsa.ExportCspBlob(false));
+			PrivateKey = Convert.ToBase64String(rsa.ExportCspBlob(true));
+		}
+
+		public byte[] DecryptData(byte[] data)
+		{
+			rsa.ImportCspBlob(Convert.FromBase64String(PrivateKey));
+			return rsa.Decrypt(data, false);
+		}
+	}
+
+	public class ClientRSA
+	{
+		private string serverPublicKey;
+
+		public ClientRSA(string serverPublicKey)
+		{
+			this.serverPublicKey = serverPublicKey;
+		}
+
+		public byte[] EncryptData(byte[] data)
+		{
+			using (var rsa = new RSACryptoServiceProvider())
+			{
+				rsa.PersistKeyInCsp = false;
+				rsa.ImportCspBlob(Convert.FromBase64String(serverPublicKey));
+				return rsa.Encrypt(data, false);
+			}
+		}
+
+		public byte[] GenerateSymmetricKey()
+		{
+			using (var aes = Aes.Create())
+			{
+				aes.GenerateKey();
+				return aes.Key;
+			}
+		}
+	}
+}
